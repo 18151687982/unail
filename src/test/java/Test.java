301ea92d6b6dc0df362cc7fd92255953395e1fb5
yuhanyuan <635918990@qq.com>
2018-09-28 16:30:53 +0800

    unail

---
+++ src/test/java/Test.java
@@ -0,0 +1,15 @@
+import com.alienlab.system.controller.SystemController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+/**
+ * Created by 橘 on 2016/7/22.
+ */
+public class Test {
+    public static void main(String [] args){
+        Date d=new Date();
+        d.setTime(1546099200000L+24*60*60*1000);
+        System.out.println(d.toLocaleString());
+    }
+}
